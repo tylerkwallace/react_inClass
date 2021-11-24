6ad2fb8c5cb6b649c950cdb0aff71a2ebe0f1779
tylerkwallace <wallace_tyler1@columbusstate.edu>
2021-11-24 17:18:16 +0000

    part 3

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+import { Button, Card, CardBody, CardTitle, Jumbotron } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      showSecondCard: false
+    }
+  }
+
+  render(){
+    let cname="lead";
+    return(
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">My First React App</h1>
+          <p className={cname}>This is my first attempt at it.</p>
+          <hr className={"my-2"} />
+          <p>Playing with reactstrap.</p>
+          <p className={cname}></p>
+        </Jumbotron>
+        <Card key="firstCard">
+          <CardBody>
+            <CardTitle>
+              My Card
+            </CardTitle>
+            <Button onClick={() => this.setState({ showSecondCard: true })}>Button</Button>
+          </CardBody>
+        </Card>
+        {this.state.showSecondCard ? <Card>
+          <CardBody>
+            <CardTitle>
+              My Second Card
+            </CardTitle>
+            <Button onClick={() => this.setState({ showSecondCard: false })}>Button</Button>
+          </CardBody>
+        </Card> : null}
+      </div>
+    )
+  }
+  // if something else something
+  // if ? true : false
 }
 
 export default App;
